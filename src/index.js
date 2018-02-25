ab2fb417c5c6648b8391e0ffc2df366e130fcb0d
Evgeni Kurilenko <evgeni.kurilenko@gmail.com>
2018-02-25 20:16:32 +0300

    Task3 solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
+  
+    //arr
+    //comparator
+    
   constructor() {
-    // your implementation
+    this.arr = [];
+    this.comparator = (a,b)=>(a - b);
   }
 
   add(element) {
+      this.arr.push(element);
     // your implementation
   }
 
   at(index) {
+      return this.arr[index];
     // your implementation
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;// your implementation
   }
 
   toArray() {
     // your implementation
+      return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+indices.sort();
+    var newArray = [];
+    indices.forEach ((index) =>{
+        newArray.push(this.arr[index]);
+    })
+    newArray.sort(this.comparator);
+    newArray.forEach ((element,index) =>{
+        this.arr[indices[index]]=element;
+    })
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;// your implementation
   }
 }
 
